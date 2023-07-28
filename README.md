729e813773ef56b6a07862c269329a5d147685c0
Santiago Hernández <santi.hergut@gmail.com>
2023-07-27 23:15:00 -0500

    built library to process imgs and runner

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Seed Segmenter
 
-### *🏗️ In progress...*
+Seed Segementer is a tool that allows you to take an image of fluorescent seeds and output the count of gene-carrier seeds (red) and "normal" seeds.
+
+It takes a batch of images and outputs in `.csv` format the count of seeds for each input image given.
 
 **Before**
 <div style="text-align:center">
@@ -13,3 +15,64 @@
 <div style="text-align:center">
     <img src="readme_imgs/segmented.png" alt="After" height="300">
 </div>
+
+## Getting started
+Seed segmenter requires the following for each input image:
+- An mCherry image with fluorescent seeds (to count gene-carrier seeds) 
+- A brightfield TL image (to count total number of seeds)
+
+<details>
+  <summary>Note on scale</summary>
+Scale bar should be around 3.4 mm. If scale is different and results are bad, then scale can be adjusted in `config.py` with the `DISTANCE_THRESHOLDS` constant.
+</details>
+<br>
+
+Images should be in a directory with a specific naming convention:
+
+`<prefix>_<image_type>.<extension>`
+- `prefix`: name of image, each image pair (mCherry + BF TL) should have the same prefix.
+- `image_type`: either `RFP` (mCherry image) or `BFTL` (brightfield TL)
+- `extension`: extension of the image (usually `.tiff`)
+
+Example `VZ254_BFTL.tif` and `VZ254_RFP.tif`
+
+## Usage
+
+First, install the required dependencies. Make sure you have at least `Python 3.7`. It is recommended to create a virtual environment first.
+```bash
+pip install -r requirements.txt
+```
+
+After installing requirements, and making sure that the images are in a directory and in the format and specification specified above, Seed Segmenter can be run with:
+```bash
+python run.py --dir ./images --output ./output_directory --store
+```
+`--dir`: directory with input images with the specified format (required).
+`--output`: output directory to store results (required)
+`--store`: flag, if present, stores the processed images with contours for counting.
+
+You can get details of all arguments by running:
+```bash
+python run.py --help
+```
+
+### Debugging
+In some cases, seeds might not be separated properly or some seeds might be left out from the final result. If this is the case, you can change parameters in `config.py`, particularly the following constants, which are set by type of image.
+
+```python
+# NOTE: This will be the distance threshold used to separate seeds that are too close together
+# Increase if seeds are closer together, decrease if seeds are farther apart
+# Balance: the smaller this is set, the more likely seeds will be separated BUT the more likely smaller (or dimmer) seeds will be left out
+DISTANCE_THRESHOLDS = {
+    'BFTL': 10,
+    'RFP': 14
+}
+
+# NOTE: This will be the ratio of 255 (max value) that will be used to threshold image to capture seeds
+# Increase if seeds are brighter, decrease if seeds are darker
+# Balance: the smaller this is set, the more likely seeds will be captured BUT the more likely noise will be captured
+INITIAL_BRIGHTNESS_THRESHOLDS = {
+    'BFTL': 0.93,
+    'RFP': 0.90
+}
+```
--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import argparse
 from datetime import datetime
 import os
 
-from seeds import process_bf, process_rfp
+from seeds import process_seed_image
 
 
 def parse_args():
@@ -10,10 +10,9 @@ def parse_args():
         "This script takes an image or directory of images and returns the number of seeds in the image(s)."
     )
     parser = argparse.ArgumentParser(description=help_message)
-    parser.add_argument('-f', '--file', type=str, help='Path to the image file')
-    parser.add_argument('-d', '--dir', type=str, help='Path to the image directory')
+    parser.add_argument('-d', '--dir', type=str, help='Path to the image directory', required=True)
     parser.add_argument('-o', '--output', type=str, help='Path to the output directory', required=True)
-    parser.add_argument('-s', '--store', action='store_true', help='Store output images')
+    parser.add_argument('-s', '--store', action='store_true', default=True, help='Store output images')
 
     return parser.parse_args()
 
@@ -24,7 +23,7 @@ def store_results(results, output_folder):
     with open(output_file, 'w') as f:
         f.write('prefix,red_seeds,dark_seeds,total_seeds\n')
         for i, result in enumerate(results):
-            f.write(f'{result["prefix"]},{result["red_seeds"]},{result["dark_seeds"]},{result["total_seeds"]}\n')
+            f.write(f'{result["prefix"]},{result.get("red_seeds")},{result.get("dark_seeds")},{result.get("total_seeds")}\n')
 
     print(f"Finished processing all files and stored results in {output_file}")
 
@@ -32,14 +31,10 @@ def store_results(results, output_folder):
 if __name__ == "__main__":
     args = parse_args()
     
-    files = []
-    if args.file is not None:
-        files.append(args.file)
-    elif args.dir is not None:
-        files = [os.path.join(args.dir, f) for f in os.listdir(args.dir) if os.path.isfile(os.path.join(args.dir, f))]
-    else:
-        raise Exception("No file or directory specified")
-    
+    files = [os.path.join(args.dir, f) for f in os.listdir(args.dir) if os.path.isfile(os.path.join(args.dir, f))]
+    # filter out non-image files
+    valid_extensions = ['.tif', '.tiff', '.png', '.jpg', '.jpeg']
+    files = [f for f in files if os.path.splitext(f)[1].lower() in valid_extensions]
     files.sort()
 
     image_files = {}
@@ -50,7 +45,7 @@ if __name__ == "__main__":
         else:
             image_files[prefix].append(file)
 
-    print(f'Found {len(image_files.keys())} images in {len(files)} files')
+    print(f'Found {len(image_files.keys())} unique prefixes in {len(files)} files')
 
     # Call the process_image function with the specified image path
     results = []
@@ -60,25 +55,25 @@ if __name__ == "__main__":
         for file in image_files[prefix]:
             filename = os.path.basename(file)
             postfix = filename.split('_')[1]
-            if postfix == 'BF':
-                print(f'\tBF image: {filename}')
-                total_seeds = process_bf(file, prefix, args.output if args.store else None)
+            if postfix == 'BFTL':
+                print(f'\tBFTL image: {filename}')
+                total_seeds = process_seed_image(file, postfix, prefix, args.output if args.store else None)
                 result['total_seeds'] = total_seeds
             elif postfix == 'RFP':
                 print(f'\tRFP image: {filename}')
-                red_seeds = process_rfp(file, prefix, args.output if args.store else None)
+                red_seeds = process_seed_image(file, postfix, prefix, args.output if args.store else None)
                 result['red_seeds'] = red_seeds
             else:
                 print(f'\tUnknown image type for {filename}')
 
-            if 'total_seeds' not in result:
-                print(f"\tCouldn't find BF image for {prefix}")
-            if 'red_seeds' not in result:    
-                print(f"\tCouldn't find RFP image for {prefix}")
+        if 'total_seeds' not in result:
+            print(f"\tCouldn't find BFTL image for {prefix}")
+        if 'red_seeds' not in result:    
+            print(f"\tCouldn't find RFP image for {prefix}")
 
-            result['dark_seeds'] = result.get('total_seeds',0) - result.get('red_seeds',0)
+        result['dark_seeds'] = result.get('total_seeds',0) - result.get('red_seeds',0)
 
-            results.append(result)
+        results.append(result)
     
     store_results(results, args.output)
 
